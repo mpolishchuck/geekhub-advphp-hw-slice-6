3b7beeae64b279d87b014c737799f2caf1eb8087
Maxim Polishchuck <mpolishchuck@gmail.com>
2014-01-08 18:48:42 +0200

    Implemented simple blog display

--- src/PaulMaxwell/BlogBundle/Controller/DefaultController.php
+++ src/PaulMaxwell/BlogBundle/Controller/DefaultController.php
@@ -8,6 +8,63 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PaulMaxwellBlogBundle:Default:index.html.twig', array());
+        $em = $this->getDoctrine()->getManager();
+
+        /**
+         * @var \PaulMaxwell\BlogBundle\Entity\ArticleRepository $ar
+         */
+        $ar = $em->getRepository('PaulMaxwellBlogBundle:Article');
+        $articles = $ar->findLastArticles(10);
+
+        return $this->render('PaulMaxwellBlogBundle:Default:index.html.twig', array(
+            'articles' => $articles,
+        ));
+    }
+
+    public function showArticleAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        /**
+         * @var \PaulMaxwell\BlogBundle\Entity\ArticleRepository $ar
+         */
+        $ar = $em->getRepository('PaulMaxwellBlogBundle:Article');
+        $article = $ar->find($id);
+
+        return $this->render('PaulMaxwellBlogBundle:Default:article.html.twig', array(
+            'article' => $article,
+        ));
+    }
+
+    public function showCategoryAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        /**
+         * @var \PaulMaxwell\BlogBundle\Entity\ArticleRepository $ar
+         */
+        $ar = $em->getRepository('PaulMaxwellBlogBundle:Article');
+        $articles = $ar->findLastArticles(10, $id);
+
+        return $this->render('PaulMaxwellBlogBundle:Default:index.html.twig', array(
+            'articles' => $articles,
+        ));
+    }
+
+    public function partialSidebarAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        /**
+         * @var \PaulMaxwell\BlogBundle\Entity\ArticleRepository $ar
+         */
+        $ar = $em->getRepository('PaulMaxwellBlogBundle:Article');
+        $last = $ar->findLastArticles(5);
+        $popular = $ar->findPopularArticles(5);
+
+        return $this->render('PaulMaxwellBlogBundle:Default:_sidebar.html.twig', array(
+            'last' => $last,
+            'popular' => $popular,
+        ));
     }
 }
--- src/PaulMaxwell/BlogBundle/Entity/ArticleRepository.php
+++ src/PaulMaxwell/BlogBundle/Entity/ArticleRepository.php
@@ -6,5 +6,25 @@ use Doctrine\ORM\EntityRepository;
 
 class ArticleRepository extends EntityRepository
 {
+    public function findLastArticles($limit, $category_id = null)
+    {
+        $queryBuilder = $this->createQueryBuilder('a')
+            ->orderBy('a.postedAt', 'DESC')
+            ->setMaxResults($limit);
+        if ($category_id !== null) {
+            $queryBuilder->where('a.category = :category_id')
+                ->setParameter(':category_id', $category_id);
+        }
 
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    public function findPopularArticles($limit)
+    {
+        $queryBuilder = $this->createQueryBuilder('a')
+            ->orderBy('a.hits', 'DESC')
+            ->setMaxResults($limit);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
 }
