996abc2f726a759d952205ca428a26113d93a15a
Maxim Polishchuck <mpolishchuck@gmail.com>
2014-01-08 15:29:47 +0200

    Added images importing by fixture

--- src/PaulMaxwell/BlogBundle/DataFixtures/ORM/LoadArticles.php
+++ src/PaulMaxwell/BlogBundle/DataFixtures/ORM/LoadArticles.php
@@ -7,10 +7,16 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use PaulMaxwell\BlogBundle\Entity\Article;
 use Symfony\Component\CssSelector\CssSelector;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class LoadArticles extends AbstractFixture implements OrderedFixtureInterface
+class LoadArticles extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
 
     /**
      * Load data fixtures with the passed EntityManager
@@ -23,11 +29,16 @@ class LoadArticles extends AbstractFixture implements OrderedFixtureInterface
         $crawler->addXmlContent(file_get_contents(__DIR__ . '/data/j2xml1250020140107183129.xml'));
 
         CssSelector::disableHtmlExtension();
-        $crawler = $crawler->filter('j2xml content');
+        $content = $crawler->filter('j2xml content');
 
         $fixture = $this;
 
-        $crawler->each(function (Crawler $node, $i) use ($fixture, $manager) {
+        $content->each(function (Crawler $node, $i) use ($fixture, $manager) {
+            if ($node->filter('state')->text() != '1') {
+                // Skip unpublished articles
+                return;
+            }
+
             $catid = html_entity_decode(
                 $node->filter('catid')->text(),
                 ENT_NOQUOTES | ENT_SUBSTITUTE,
@@ -67,6 +78,29 @@ class LoadArticles extends AbstractFixture implements OrderedFixtureInterface
         });
 
         $manager->flush();
+
+        $images = $crawler->filter('j2xml img');
+        $images->each(function (Crawler $node, $i) use ($fixture) {
+            $filename = $node->attr('src');
+            $content = trim($node->text());
+
+            if (is_file($fixture->container->getParameter('paul_maxwell_blog.images_location') . '/' . $filename)) {
+                return;
+            }
+
+            if (!is_dir($fixture->container->getParameter('paul_maxwell_blog.images_location') . '/' . dirname($filename))) {
+                mkdir(
+                    $fixture->container->getParameter('paul_maxwell_blog.images_location') . '/' . dirname($filename),
+                    0777,
+                    true
+                );
+            }
+
+            file_put_contents(
+                $fixture->container->getParameter('paul_maxwell_blog.images_location') . '/' . $filename,
+                base64_decode($content)
+            );
+        });
     }
 
     /**
@@ -77,5 +111,17 @@ class LoadArticles extends AbstractFixture implements OrderedFixtureInterface
     function getOrder()
     {
         return 2;
+    }
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     *
+     * @api
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }
