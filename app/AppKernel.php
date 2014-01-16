7885db7eb53410298a482157e6db62105383ea61
Maxim Polishchuck <mpolishchuck@gmail.com>
2014-01-16 20:36:44 +0200

    Installed SonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,12 @@ class AppKernel extends Kernel
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
             new Lunetics\LocaleBundle\LuneticsLocaleBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
